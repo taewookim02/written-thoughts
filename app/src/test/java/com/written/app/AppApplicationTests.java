6afc24964fd13e0d40a4778066f92376d73676c1
Tae Woo Kim <taewookim02@gmail.com>
2024-06-24 15:13:03 +0900

    chore: add basic project setup

---
+++ app/src/test/java/com/written/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.written.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
