ed6e344d0ad1f077b9b03ad79c3ea2efcbf4783f
Tae Woo Kim <taewookim02@gmail.com>
2024-06-24 16:57:26 +0900

    Add basic mybatis query

--- app/src/main/java/com/written/app/member/controller/MemberController.java
+++ app/src/main/java/com/written/app/member/controller/MemberController.java
@@ -1,15 +1,24 @@
 package com.written.app.member.controller;
 
+import com.written.app.member.service.MemberService;
+import com.written.app.member.vo.MemberVo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("member")
+@RequiredArgsConstructor
 public class MemberController {
 
-    @GetMapping("login")
-    public void Login() {
+    private final MemberService service;
 
+    @GetMapping("login")
+    public MemberVo login(MemberVo vo) {
+        System.out.println("vo = " + vo);
+        // print member
+        MemberVo loginMemberVo = service.login(vo);
+        return loginMemberVo;
     }
 }
--- app/src/main/java/com/written/app/member/dao/MemberDao.java
+++ app/src/main/java/com/written/app/member/dao/MemberDao.java
@@ -1,7 +1,15 @@
 package com.written.app.member.dao;
 
+import com.written.app.member.mapper.MemberMapper;
+import com.written.app.member.vo.MemberVo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberDao {
+    private final MemberMapper mapper;
+    public MemberVo login(MemberVo vo) {
+        return mapper.login(vo);
+    }
 }
