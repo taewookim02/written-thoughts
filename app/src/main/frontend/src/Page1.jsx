768a5dc6a8cef9b3ded9a7ae1457bb9d1979bc76
Tae Woo Kim <taewookim02@gmail.com>
2024-06-26 13:24:02 +0900

    Add react-router-dom

---
+++ app/src/main/frontend/src/Page1.jsx
@@ -0,0 +1,5 @@
+function Page1() {
+  return <h1>page1</h1>;
+}
+
+export default Page1;
--- app/src/main/frontend/src/index.js
+++ app/src/main/frontend/src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Page1 from "./Page1";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/page1" element={<Page1 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
