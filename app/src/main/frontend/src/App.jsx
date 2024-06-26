b56bb86bc1d08e06000fb0621ee024f602538bde
Tae Woo Kim <taewookim02@gmail.com>
2024-06-26 18:07:57 +0900

    Add pages and react routers

--- app/src/main/frontend/src/App.jsx
+++ app/src/main/frontend/src/App.jsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from "react";
-
-function App() {
-  const [hello, setHello] = useState("");
-  const fetchData = async () => {
-    try {
-      const res = await fetch("http://localhost:8080/app/api/test", {
-        mode: "cors",
-      });
-      if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}`);
-      }
-      const data = await res.json();
-      setHello(data.message);
-    } catch (error) {
-      console.log("err:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+const App = () => {
+  // introductory page
   return (
     <div className="App">
-      <p>{hello ? hello : "loading.."}</p>
+      <p>hello</p>
     </div>
   );
-}
+};
 
 export default App;
--- app/src/main/frontend/src/Page1.jsx
+++
@@ -1,5 +0,0 @@
-function Page1() {
-  return <h1>page1</h1>;
-}
-
-export default Page1;
