7ca7a8413f0f87a63dee7da11635cfada2c4e7e2
Tae Woo Kim <taewookim02@gmail.com>
2024-06-26 13:09:54 +0900

    Add fetch and crossorigin

--- app/src/main/frontend/src/App.js
+++ app/src/main/frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [hello, setHello] = useState("");
+  const fetchData = async () => {
+    try {
+      const res = await fetch("http://localhost:8080/app/api/test", {
+        mode: "cors",
+      });
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const data = await res.json();
+      setHello(data.message);
+    } catch (error) {
+      console.log("err:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{hello ? hello : "loading.."}</p>
     </div>
   );
 }
