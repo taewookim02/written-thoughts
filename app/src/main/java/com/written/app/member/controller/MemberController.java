818be332114b2379c6cf6fa4eb799185a2e257ff
Tae Woo Kim <taewookim02@gmail.com>
2024-06-27 10:40:45 +0900

    feat: Add cors filter

--- app/src/main/java/com/written/app/member/controller/MemberController.java
+++ app/src/main/java/com/written/app/member/controller/MemberController.java
@@ -4,15 +4,21 @@ import com.written.app.member.service.MemberService;
 import com.written.app.member.vo.MemberVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("member")
+@RequestMapping("/api/v1/member")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService service;
+
+    @PostMapping("login")
+    public String login() {
+        return "login from controller";
+    }
 
 //    @GetMapping("login")
 //    public MemberVo login(MemberVo vo) {
