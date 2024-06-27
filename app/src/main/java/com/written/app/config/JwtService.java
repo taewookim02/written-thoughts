d3b0b435546ac2417c82f77adfa7dd66ff3d57fe
Tae Woo Kim <taewookim02@gmail.com>
2024-06-27 18:03:52 +0900

    feat: add extractClaims, signInKey, and jwt secret key

--- app/src/main/java/com/written/app/config/JwtService.java
+++ app/src/main/java/com/written/app/config/JwtService.java
@@ -1,10 +1,34 @@
 package com.written.app.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    // TODO: application.yml secret key
+    private static final String SECRET_KEY = "66790f1b278f5f70d67b7ca193ae5f597642c504b0351c145e7215f4ae13718f";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
