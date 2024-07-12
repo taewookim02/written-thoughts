bc64a20c17f99d3075e34dc5437f4e08e25c5692
Tae Woo Kim <taewookim02@gmail.com>
2024-07-13 01:37:42 +0900

    chore: remove cached sqlWorkspace directory

--- sqlWorkspace/00_CREATE_USER.sql
+++
@@ -1,8 +0,0 @@
--- Create a new user with a password
-CREATE USER spring_user WITH PASSWORD '1234';
-
--- Create a new database owned by the new user
-CREATE DATABASE spring_db OWNER spring_user;
-
--- Grant all privileges on the database to the user
-GRANT ALL PRIVILEGES ON DATABASE spring_db TO spring_user;
