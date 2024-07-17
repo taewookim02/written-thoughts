3ec8a6fda0446c471e251d4db3f3fe35fb776fd1
Tae Woo Kim <taewookim02@gmail.com>
2024-07-17 22:41:56 +0900

    feat: implement checking of valid token on AuthFilter

--- backend/src/main/java/com/written/app/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/written/app/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.written.app.config;
 
+import com.written.app.repository.TokenRepository;
 import com.written.app.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -23,6 +24,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
 
     @Override
     protected void doFilterInternal(
@@ -42,7 +44,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            var isTokenValid = tokenRepository.findByToken(jwt)
+                    .map(token -> !token.isExpired() && !token.isRevoked())
+                    .orElse(false);
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
