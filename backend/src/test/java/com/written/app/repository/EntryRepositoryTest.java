25258b883449c5cac726134bd00b4e209702af72
Tae Woo Kim <taewookim02@gmail.com>
2024-07-22 16:20:13 +0900

    refactor: use @BeforeEach for user creation

--- backend/src/test/java/com/written/app/repository/EntryRepositoryTest.java
+++ backend/src/test/java/com/written/app/repository/EntryRepositoryTest.java
@@ -1,18 +1,19 @@
 package com.written.app.repository;
 
-import com.github.javafaker.Pokemon;
 import com.written.app.model.Entry;
 import com.written.app.model.Label;
 import com.written.app.model.Role;
 import com.written.app.model.User;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest // default: rollback each transaction after method ends
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class EntryRepositoryTest {
 
@@ -23,19 +24,25 @@ public class EntryRepositoryTest {
     @Autowired
     private LabelRepository labelRepository;
 
-    @Test
-    public void testSave() {
-        // given
-        User user = User.builder()
+    // declare testUser as class field for use
+    private User testUser;
+
+    @BeforeEach
+    void setUp() {
+        testUser = User.builder()
                 .email("test@example.com")
                 .password("password")
                 .role(Role.USER)
                 .build();
-        user = userRepository.save(user);
+        testUser = userRepository.save(testUser);
+    }
 
+    @Test
+    public void EntryRepository_Save_ReturnEntry() {
+        // given
         Label label = Label.builder()
                 .name("Test Label")
-                .user(user)
+                .user(testUser)
                 .build();
         label = labelRepository.save(label);
 
@@ -44,42 +51,56 @@ public class EntryRepositoryTest {
                 .title("Title01")
                 .content("Content\n01")
                 .label(label) // fk
-                .user(user) // fk
+                .user(testUser) // fk
                 .build();
 
         // when
         Entry savedEntry = entryRepository.save(entry);
 
         // then
-        Assertions.assertThat(savedEntry).isNotNull();
-        Assertions.assertThat(savedEntry.getId()).isGreaterThan(0);
-        Assertions.assertThat(savedEntry.getUser()).isEqualTo(user);
-        Assertions.assertThat(savedEntry.getLabel()).isEqualTo(label);
+        assertThat(savedEntry).isNotNull();
+        assertThat(savedEntry.getId()).isGreaterThan(0);
+        assertThat(savedEntry.getUser()).isEqualTo(testUser);
+        assertThat(savedEntry.getLabel()).isEqualTo(label);
     }
 
-    @Test
-    public void testSaveWithoutLabel() {
-        // given
-        User user = User.builder()
-                .email("test@example.com")
-                .password("password")
-                .role(Role.USER)
-                .build();
-        user = userRepository.save(user);
 
+    @Test
+    public void EntryRepository_SaveWithoutLabel_ReturnEntry() {
+        // given
         Entry entry = Entry.builder()
                 .title("Title01")
                 .content("Content01")
-                .user(user)
+                .user(testUser)
                 .build();
 
         // when
         Entry savedEntry = entryRepository.save(entry);
 
         // then
-        Assertions.assertThat(savedEntry).isNotNull();
-        Assertions.assertThat(savedEntry.getId()).isGreaterThan(0);
-        Assertions.assertThat(savedEntry.getUser()).isEqualTo(user);
-        Assertions.assertThat(savedEntry.getLabel()).isNull();
+        assertThat(savedEntry).isNotNull();
+        assertThat(savedEntry.getId()).isGreaterThan(0);
+        assertThat(savedEntry.getUser()).isEqualTo(testUser);
+        assertThat(savedEntry.getLabel()).isNull();
     }
+
+    @Test
+    public void EntryRepository_FindAllByUserIdOrderByCreatedAtDesc_ReturnEntryList() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    public void EntryRepository_FindById_ReturnEntry() {
+        // given
+
+        // when
+
+        // then
+    }
+
+
 }
