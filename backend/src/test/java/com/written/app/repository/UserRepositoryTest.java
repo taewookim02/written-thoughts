d2c63eff664c3c6c3251928a784f16db7fdfcc8a
Tae Woo Kim <taewookim02@gmail.com>
2024-07-25 06:40:40 +0900

    fix: entry findAllByUserIdOrderByCreatedAtDesc test

---
+++ backend/src/test/java/com/written/app/repository/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.written.app.repository;
+
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class UserRepositoryTest {
+
+//    @Test
+//    public void UserRepository_
+
+}
