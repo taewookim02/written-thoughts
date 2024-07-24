db751afb31e00e63390cfd9c6d282b8cf2d31ba5
Tae Woo Kim <taewookim02@gmail.com>
2024-07-24 18:04:32 +0900

    test: label findById

--- backend/src/test/java/com/written/app/repository/LabelRepositoryTest.java
+++ backend/src/test/java/com/written/app/repository/LabelRepositoryTest.java
@@ -48,6 +48,38 @@ public class LabelRepositoryTest {
     }
 
 
+    @Test
+    public void LabelRepository_FindById_ReturnLabel() {
+        // given
+        User user = User.builder()
+                .email("test@example.com")
+                .password("password")
+                .createdAt(LocalDateTime.now())
+                .role(Role.USER)
+                .build();
+        user = userRepository.save(user);
+
+        Label label = Label.builder()
+                .name("test label")
+                .user(user)
+                .build();
+        label = labelRepository.save(label);
+
+        Label label2 = Label.builder()
+                .name("test label2")
+                .user(user)
+                .build();
+        label2 = labelRepository.save(label2);
+
+
+        // when
+        Label resultLabel = labelRepository.findById(label2.getId()).get();
+
+        // then
+        assertThat(resultLabel).isNotNull();
+        assertThat(resultLabel.getName()).isEqualTo("test label2");
+        assertThat(resultLabel.getUser()).isEqualTo(user);
+    }
 
 
 }
