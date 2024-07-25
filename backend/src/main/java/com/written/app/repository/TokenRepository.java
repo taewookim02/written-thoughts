c20d3635d6c6a5c2cd8dc00db511a183f16ab027
Tae Woo Kim <taewookim02@gmail.com>
2024-07-25 20:44:11 +0900

    fix: findAllValidTokens logic error

--- backend/src/main/java/com/written/app/repository/TokenRepository.java
+++ backend/src/main/java/com/written/app/repository/TokenRepository.java
@@ -11,7 +11,7 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
 
     @Query("""
             select t from Token t inner join User u on t.user.id = u.id
-            where u.id = :userId and (t.expired = false or t.revoked = false)
+            where u.id = :userId and (t.expired = false and t.revoked = false)
             """)
     List<Token> findAllValidTokensByUser(Integer userId);
 
