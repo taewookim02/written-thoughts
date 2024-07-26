0af6c6de24fd4545ffd9d3b5f6c9c8aacf269b46
Tae Woo Kim <taewookim02@gmail.com>
2024-07-27 00:43:26 +0900

    test: list service create

--- backend/src/test/java/com/written/app/service/ListServiceTest.java
+++ backend/src/test/java/com/written/app/service/ListServiceTest.java
@@ -1,5 +1,10 @@
 package com.written.app.service;
 
+import com.written.app.dto.EntryDto;
+import com.written.app.dto.ListDto;
+import com.written.app.mapper.ListMapper;
+import com.written.app.model.Entry;
+import com.written.app.model.Label;
 import com.written.app.model.Role;
 import com.written.app.model.User;
 import com.written.app.repository.ListRepository;
@@ -14,6 +19,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -62,5 +68,37 @@ public class ListServiceTest {
 
         verify(listRepository).findAllByUserId(user.getId());
     }
+
+    @Test
+    public void ListService_Create_ReturnListDto() {
+        // given
+        Integer listId = 1;
+        UsernamePasswordAuthenticationToken authToken = mock(UsernamePasswordAuthenticationToken.class);
+        when(authToken.getPrincipal()).thenReturn(user);
+
+        ListDto listDto = new ListDto(listId, user.getId(), "List01");
+
+        com.written.app.model.List list = com.written.app.model.List.builder()
+                .user(user)
+                .title(listDto.title())
+                .build();
+
+        when(listRepository.save(any(com.written.app.model.List.class))).thenAnswer(invocation -> {
+            com.written.app.model.List savedList = invocation.getArgument(0);
+            savedList.setId(1);
+            return savedList;
+        });
+
+        // when
+        ListDto resultDto = listService.create(listDto, authToken);
+
+        // then
+        assertThat(resultDto).isNotNull();
+        assertThat(resultDto.title()).isEqualTo("List01");
+        assertThat(resultDto.userId()).isEqualTo(1);
+
+        verify(listRepository).save(any(com.written.app.model.List.class));
+    }
+
 
 }
