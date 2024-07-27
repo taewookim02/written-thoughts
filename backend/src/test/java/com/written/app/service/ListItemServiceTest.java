91e0b5fa86a92de8dc7fdc4bc007e064fb8f2c68
Tae Woo Kim <taewookim02@gmail.com>
2024-07-27 12:55:55 +0900

    test: list item service create

--- backend/src/test/java/com/written/app/service/ListItemServiceTest.java
+++ backend/src/test/java/com/written/app/service/ListItemServiceTest.java
@@ -1,12 +1,28 @@
 package com.written.app.service;
 
+import com.written.app.dto.ListItemDto;
+import com.written.app.model.List;
+import com.written.app.model.ListItem;
+import com.written.app.model.Role;
+import com.written.app.model.User;
 import com.written.app.repository.ListItemRepository;
 import com.written.app.repository.ListRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.nio.file.AccessDeniedException;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ListItemServiceTest {
@@ -18,9 +34,54 @@ public class ListItemServiceTest {
     @InjectMocks
     private ListItemService listItemService;
 
-    @Test
-    public void ListItemService_Create_ReturnListItemDto() {
+    private User user;
 
+    @BeforeEach
+    public void setUp() {
+        user = User.builder()
+                .id(1)
+                .email("test@example.com")
+                .password("password")
+                .role(Role.USER)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    @Test
+    public void ListItemService_Create_ReturnListItemDto() throws AccessDeniedException {
+        // given
+        UsernamePasswordAuthenticationToken authToken = mock(UsernamePasswordAuthenticationToken.class);
+        when(authToken.getPrincipal()).thenReturn(user);
+
+        Integer listId = 1;
+        Integer listItemId = 1;
+        ListItemDto inputDto = new ListItemDto(null, "Test content", listId);
+        List list = List.builder()
+                .id(listId)
+                .title("List01")
+                .user(user)
+                .build();
+        when(listRepository.findById(listId)).thenReturn(Optional.of(list));
+
+        ListItem savedListItem = ListItem.builder()
+                .id(listItemId)
+                .content("Test content")
+                .list(list)
+                .build();
+        when(listItemRepository.save(any(ListItem.class))).thenReturn(savedListItem);
+
+
+        // when
+        ListItemDto resultDto = listItemService.create(inputDto, authToken);
+
+        // then
+        assertThat(resultDto).isNotNull();
+        assertThat(resultDto.id()).isEqualTo(listItemId);
+        assertThat(resultDto.listId()).isEqualTo(listId);
+        assertThat(resultDto.content()).isEqualTo("Test content");
+
+        verify(listRepository).findById(listId);
+        verify(listItemRepository).save(any(ListItem.class));
     }
 
 
