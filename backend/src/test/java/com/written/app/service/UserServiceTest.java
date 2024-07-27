423a56f48fbbd4f066390d69f8c5f64953df06a3
Tae Woo Kim <taewookim02@gmail.com>
2024-07-27 16:48:16 +0900

    test: user service delete

---
+++ backend/src/test/java/com/written/app/service/UserServiceTest.java
@@ -0,0 +1,54 @@
+package com.written.app.service;
+
+import com.written.app.model.Role;
+import com.written.app.model.User;
+import com.written.app.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
+    private UserService userService;
+
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = User.builder()
+                .id(1)
+                .email("test@example.com")
+                .password("password")
+                .role(Role.USER)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    @Test
+    public void UserService_Delete_ReturnVoid() {
+        // given
+        UsernamePasswordAuthenticationToken authToken = mock(UsernamePasswordAuthenticationToken.class);
+        when(authToken.getPrincipal()).thenReturn(user);
+
+        // when
+        userService.delete(authToken);
+
+        // then
+        verify(userRepository).delete(user);
+    }
+}
