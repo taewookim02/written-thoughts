d85c603f1eff717e6d4588d59e33338117096749
Tae Woo Kim <taewookim02@gmail.com>
2024-07-28 23:52:53 +0900

    test: authenticationController refreshToken

--- backend/src/test/java/com/written/app/controller/AuthenticationControllerTest.java
+++ backend/src/test/java/com/written/app/controller/AuthenticationControllerTest.java
@@ -6,6 +6,8 @@ import com.written.app.dto.AuthenticationRequest;
 import com.written.app.dto.AuthenticationResponse;
 import com.written.app.dto.RegisterRequest;
 import com.written.app.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,15 +17,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(
         controllers = AuthenticationController.class,
@@ -93,7 +94,36 @@ public class AuthenticationControllerTest {
                 .andExpect(jsonPath("$.access_token").value("jwtToken"))
                 .andExpect(jsonPath("$.refresh_token").value("refreshToken"));
         verify(authenticationService).authenticate(inputRequest);
+    }
 
+    @Test
+    public void AuthenticationController_RefreshToken_ReturnAuthResponse() throws Exception {
+        // given
+        String refreshToken = "validRefreshToken";
+        AuthenticationResponse authResponse = AuthenticationResponse.builder()
+                .accessToken("newAccessToken")
+                .refreshToken("newRefreshToken")
+                .build();
+
+        // mock the behavior of `AuthenticationService.refreshToken(req, res)`
+        doAnswer(invocation -> {
+            HttpServletResponse response = invocation.getArgument(1);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+            return null;
+        }).when(authenticationService).refreshToken(any(HttpServletRequest.class), any(HttpServletResponse.class));
+
+        // when
+        ResultActions response = mockMvc.perform(post("/auth/refresh-token")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + refreshToken)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        response.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.access_token").value("newAccessToken"))
+                .andExpect(jsonPath("$.refresh_token").value("newRefreshToken"));
+        verify(authenticationService).refreshToken(any(HttpServletRequest.class), any(HttpServletResponse.class));
 
     }
 
