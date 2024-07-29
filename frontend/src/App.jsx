507bfd3b6a768e95cb81601126cfa8d8f7045ab0
Tae Woo Kim <taewookim02@gmail.com>
2024-07-29 11:19:01 +0900

    refactor: use browser router on App instead of index

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,17 @@
 import Navbar from "./components/Navbar";
-
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Journal from "./pages/Journal";
+import Home from "./pages/Home";
 const App = () => {
-  const fetchData = async () => {
-    const res = await fetch("http://127.0.0.1:8080/app/api/test");
-    const data = await res.json();
-    console.log(data);
-  };
-  // fetchData();
-  // introductory page
   return (
-    <>
-      <Navbar />
-      <div className="App text-3xl font-bold underline">
-        <p>hello</p>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/journal" element={<Journal />} />
+    </Routes>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Journal from "./pages/Journal";
+import { BrowserRouter } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/journal" element={<Journal />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
