8adc08b7fe69cae559eb8aaae9e9bffe2f36071c
Tae Woo Kim <taewookim02@gmail.com>
2024-07-29 17:37:07 +0900

    feat: add register fetch api

--- frontend/src/components/features/auth/LoginForm.jsx
+++ frontend/src/components/features/auth/LoginForm.jsx
@@ -3,12 +3,27 @@ import InputField from "../../common/InputField";
 import { StyledForm } from "../../common/StyledForm";
 import { StyledSubmitButton } from "../../common/StyledSubmitButton";
 
+const handleLogin = (e) => {
+  e.preventDefault();
+  console.log("hello");
+};
+
 const LoginForm = () => {
   return (
-    <StyledForm>
+    <StyledForm onSubmit={handleLogin}>
       <h1>Login</h1>
-      <InputField type="email" name="email" placeholder="Email" />
-      <InputField type="password" name="password" placeholder="Password" />
+      <InputField
+        type="email"
+        name="email"
+        placeholder="Email"
+        required={true}
+      />
+      <InputField
+        type="password"
+        name="password"
+        placeholder="Password"
+        required={true}
+      />
       <StyledSubmitButton type="submit">Login</StyledSubmitButton>
     </StyledForm>
   );
--- frontend/src/components/features/auth/RegisterForm.jsx
+++ frontend/src/components/features/auth/RegisterForm.jsx
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledForm } from "../../common/StyledForm";
 import InputField from "../../common/InputField";
 import { StyledSubmitButton } from "../../common/StyledSubmitButton";
 
 const RegisterForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Password and confirm password do not match");
+      return;
+    }
+    const url = "http://127.0.0.1:8080/api/v1/auth/register";
+    const payload = {
+      email,
+      password,
+      confirmPassword,
+    };
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    const data = await response.json();
+    // TODO: add data.access_token and data.refresh_token somewhere
+    // TODO: handle status 409 (UserAlreadyExists)
+  };
+
   return (
-    <StyledForm>
+    <StyledForm onSubmit={handleRegister}>
       <h1>Register</h1>
-      <InputField type="email" name="email" placeholder="Email" />
-      <InputField type="password" name="password" placeholder="Password" />
+      <InputField
+        type="email"
+        name="email"
+        placeholder="Email"
+        required={true}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <InputField
+        type="password"
+        name="password"
+        placeholder="Password"
+        required={true}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <InputField
         type="password"
         name="confirmPassword"
         placeholder="Confirm password"
+        required={true}
+        onChange={(e) => setConfirmPassword(e.target.value)}
       />
       <StyledSubmitButton type="submit">Register</StyledSubmitButton>
     </StyledForm>
